81bd3c64ba2ccd1cd2e42b1b5cad8050e0c02762
Dmitry <grammattika@gmail.com>
2023-08-12 14:05:58 +0300

    Test

---
+++ SocialNetwork.Tests/UserServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using System;
+
+namespace SocialNetwork.Tests
+{
+    [TestFixture]
+    public class UserServiceTests
+    {
+        UserService userService;
+        [SetUp]
+        public void SetUp()
+        {
+            userService = new UserService();
+        }
+        [Test]
+        public void RegisterShouldThrowExceptionsIfInputIsIncorrect()
+        {
+            Assert.Throws<ArgumentNullException>(() => userService.Register(new UserRegistrationData()
+            {
+                FirstName = "Test",
+                LastName = "Test",
+                Password = "",
+                Email = "Test@gmail.com"
+            }));
+        }
+    }
+}
